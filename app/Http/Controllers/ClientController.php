5ce8f60113f92a9a3c30fcb90c9d5d0ba14762f8
“aartysk” <aartysk@gmail.com>
2022-05-21 21:45:54 +0300

    Homework

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,13 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use Illuminate\View\View;
+use Illuminate\http\Request;
 
 class ClientController extends Controller
 {
-    public function show()
+    public function index(): View
   
     {
-        $post = Client::get('name');
-        dd($post);
+        return view('Clients.index', [
+            'clients' => Client::get(),
+        ]);
     }
+
+    public function create(): View
+    {
+        return view('Clients.create');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+    public function edit(Client $client): View
+    {
+        return view('Clients.edit');
+    }
+
+    public function update()
+    {
+
+        
+    }
+
+    public function show(Client $client): View
+    {
+        return view('Clients.show');
+    }
+
+    public function destroy()
+    {
+
+        
+    }
+
 }
---
+++ resources/views/Clients/create.blade.php
@@ -0,0 +1,7 @@
+<form action="clients/create" method="post"> 
+    @csrf
+    
+    Name: <input type="text" name="name"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit">
+    </form>
